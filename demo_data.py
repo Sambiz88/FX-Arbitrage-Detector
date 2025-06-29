7e52de45e362672ec87523876d6e9d3442390a17
Sambiz88 <samygrant04@gmail.com>
2025-06-28 22:13:37 -0400

    Remove unnecessary python file

--- demo_data.py
+++
@@ -1,7 +0,0 @@
-def get_sample_rates():
-    # Matrix of direct rates between currencies (deliberately inconsistent for arbitrage)
-    return {
-        "USD": {"USD": 1.0, "EUR": 0.9, "GBP": 0.8},
-        "EUR": {"USD": 1.12, "EUR": 1.0, "GBP": 0.89},
-        "GBP": {"USD": 1.25, "EUR": 1.1, "GBP": 1.0},
-    }
